a6c80ef7e51d136a110251bd9d7a378fae6b2fa7
Chris Skinner <c.skinner@blueyonder.co.uk>
2017-04-28 22:28:02 +0100

    Add exercise comments

--- js/exercises/chapter-2.js
+++ js/exercises/chapter-2.js
@@ -1,36 +1,62 @@
 // Excercise 1
 
-// for (let i = '#'; i.length < 8; i+= '#') {
-//    console.log(i);
-// }
+// Write a loop that makes seven calls to console.log to output the following triangle:
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+
+for (let i = '#'; i.length < 8; i+= '#') {
+   console.log(i);
+}
 
 // Excercise 2
 
-// for (let i = 0; i <= 100; i++) {
-//    let output = '';
-//    if ( i % 3 == 0 ) {
-//       output +='Fizz';
-//    } if ( i % 5 == 0 ) {
-//       output +='Buzz';
-//    }
-//    console.log(output || i);
-// }
+// Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
+//
+// When you have that working, modify your program to print "FizzBuzz", for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
+
+for (let i = 0; i <= 100; i++) {
+   let output = '';
+   if ( i % 3 == 0 ) {
+      output +='Fizz';
+   } if ( i % 5 == 0 ) {
+      output +='Buzz';
+   }
+   console.log(output || i);
+}
 
 // Excercise 3
 
-// const sizeOfChessboard = 8;
-// let chessboard = '';
+// Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a “#” character. The characters should form a chess board.
 //
-// // Outter loop
-// for (let i = 0; i < sizeOfChessboard; i++) {
-//    // Inner loop
-//    for (let j = 0; j < sizeOfChessboard; j++) {
-//       if ( (i + j) % 2 == 0) {
-//          chessboard += ' ';
-//       } else {
-//          chessboard += '#';
-//       }
-//    }
-//    chessboard += '\n';
-// }
-// console.log(chessboard);
+// Passing this string to console.log should show something like this:
+//
+//  # # # #
+// # # # #
+//  # # # #
+// # # # #
+//  # # # #
+// # # # #
+//  # # # #
+// # # # #
+
+const sizeOfChessboard = 8;
+let chessboard = '';
+
+// Outter loop
+for (let i = 0; i < sizeOfChessboard; i++) {
+   // Inner loop
+   for (let j = 0; j < sizeOfChessboard; j++) {
+      if ( (i + j) % 2 == 0) {
+         chessboard += ' ';
+      } else {
+         chessboard += '#';
+      }
+   }
+   chessboard += '\n';
+}
+console.log(chessboard);
