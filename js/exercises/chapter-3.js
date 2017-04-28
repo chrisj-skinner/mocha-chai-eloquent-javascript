25427abbfe419e2f568198d55327394059e97b2f
Chris Skinner <c.skinner@blueyonder.co.uk>
2017-04-28 22:27:18 +0100

    Add new exercises

--- js/exercises/chapter-3.js
+++ js/exercises/chapter-3.js
@@ -1,7 +1,25 @@
 // Excercise 1
+// Write a function min that takes two arguments and returns their minimum.
 
+function min(a, b){
+   return (a > b) ? b : a;
+}
 
 // Excercise 2
-
+function isEven(num){
+   if (num === 0) {
+      return true;
+   } else if (num === 1) {
+      return false;
+   } else {
+      return isEven(num - 2);
+   }
+}
 
 // Excercise 3
+var chapter3 = {
+   min: min,
+   isEven: isEven
+};
+
+module.exports.chapter3 = chapter3;
