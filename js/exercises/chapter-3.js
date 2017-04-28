6c291a3e09ff1a322e07c00bd3b33b5dddfa1070
Chris Skinner <c.skinner@blueyonder.co.uk>
2017-04-28 23:06:09 +0100

    Add exercise 4 along with tests

--- js/exercises/chapter-3.js
+++ js/exercises/chapter-3.js
@@ -17,12 +17,31 @@ function isEven(num){
 }
 
 // Excercise 3
+function countBs(string){
+   let count = 0;
 
+   for (let i = 0; i < string.length; i++) {
+      (string[i] === 'B') ? count++ : null ;
+   }
+   return count;
+}
+
+// Excercise 4
+function countChar(string, char){
+   let count = 0;
+
+   for (let i = 0; i < string.length; i++) {
+      (string[i] === char) ? count++ : null ;
+   }
+   return count;
+}
 
 // Create object to export
 const chapter3 = {
    min: min,
-   isEven: isEven
+   isEven: isEven,
+   countBs: countBs,
+   countChar: countChar
 };
 
 module.exports.chapter3 = chapter3;
--- test/main_test.js
+++ test/main_test.js
@@ -8,13 +8,15 @@ describe('Chapter 2', function(){
 
 // Chapter 3
 describe('Chapter 3', function(){
-   
+
    // Require
    const chapter3 = require('../js/exercises/chapter-3.js').chapter3;
 
    // Setup
    const min = chapter3.min,
-      isEven = chapter3.isEven;
+      isEven = chapter3.isEven,
+      countBs = chapter3.countBs,
+      countChar = chapter3.countChar;
 
    // Exercise 1
    it('Exercise 1 should return the minimum numbered value of 2 arguments.', function(){
@@ -30,6 +32,24 @@ describe('Chapter 3', function(){
    });
    it('Exercise 2 should throw a \'call stack exceeded\' error for the value of -1.', function(){
       expect(isEven.bind(null, -1)).to.throw(RangeError);
+   });
+
+   // Exercise 3
+   it('Exercise 3 should return a value of 1 with the following param "Baby".', function(){
+      expect(countBs('Baby')).to.equal(1);
+   });
+
+   it('Exercise 3 should return a value of 2 with the following param "BABY".', function(){
+      expect(countBs('BABY')).to.equal(2);
+   });
+
+   // Exercise 4
+   it('Exercise 4 should return a value of 2 with the following params "Taller", "l"', function(){
+      expect(countChar('Taller', 'l')).to.equal(2);
+   });
+
+   it('Exercise 4 should return a value of 1 with the following params "Taller", "T".', function(){
+      expect(countChar('Taller', 'T')).to.equal(1);
    });
 
 });
